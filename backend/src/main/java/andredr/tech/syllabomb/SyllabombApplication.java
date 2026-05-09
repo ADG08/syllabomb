6de1bdc1bc68a718b35f8f85a66d97642e148fbd
ADG08 <dominguesramosandre@gmail.com>
2026-05-09 19:49:15 +0200

    chore: init spring boot project with hexagonal architecture

---
+++ backend/src/main/java/andredr/tech/syllabomb/SyllabombApplication.java
@@ -0,0 +1,13 @@
+package andredr.tech.syllabomb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SyllabombApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SyllabombApplication.class, args);
+	}
+
+}
